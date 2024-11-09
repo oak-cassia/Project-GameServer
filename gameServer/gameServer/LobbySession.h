f57a0a556ad03681d8dbbe225d55aa1fb4009e57
oak-cassia <52772732+oak-cassia@users.noreply.github.com>
2024-11-09 17:54:18 +0900

    Update LobbySession.h

--- gameServer/gameServer/LobbySession.h
+++ gameServer/gameServer/LobbySession.h
@@ -6,31 +6,20 @@ class LobbySession
 
 public:
 	LobbySession(boost::asio::io_context& io_context);
-
 	~LobbySession();
 
-	
 	void Start();
-
 	void RegisterSend(Protocol::S_GameEnd& packet);
 
 private:
 
-	
-
 	void Connect();
 	void AfterConnect(const boost::system::error_code& error);
-
 	void RegisterReceive();
-
-	
-
 	void AfterSend(const boost::system::error_code& error, size_t bytes_transferred, char* sendBuffer);
-
 	void AfterReceive(const boost::system::error_code& error, size_t length);
 
 	boost::asio::ip::tcp::endpoint endpoint;
-
 	boost::asio::ip::tcp::socket _socket;
 	array<char, MAX_RECEIVE_BUFFER_LEN> _receiveBuffer;
 	
